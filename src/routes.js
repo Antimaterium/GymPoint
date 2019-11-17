f88bb28e9f20c6d8ac49ea6dc5c9d5fac1f13f88
Antimaterium <vitorafonso33@gmail.com>
2019-11-17 00:38:51 -0200

    add students create/update features

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.index);
+
+routes.post('/students', auth, StudentController.store);
+routes.put('/students/:id', auth, StudentController.update);
 
 export default routes;
