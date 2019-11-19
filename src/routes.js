ab5e28561440300583631dfd87ed07ad269cb0cb
WIBSA-PC145 <wibsa-pc145@WIBSA-PC145s-Mac-mini.local>
2019-11-19 20:28:17 -0300

    Finalizing challange (Incomplete)

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import CheckinController from './app/controllers/CheckinController';
+import HelperController from './app/controllers/HelperController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import RegistrationController from './app/controllers/RegistrationController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+routes.get('/students/:id/checkins', CheckinController.store);
+
+routes.get('/students/:id/help-orders', HelperController.index);
+routes.post('/students/:id/help-orders', HelperController.store);
 
 routes.post('/sessions', SessionController.index);
 
@@ -17,5 +25,10 @@ routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
 routes.put('/plans/:id([0-9]+)', PlanController.update);
 routes.delete('/plans/:id([0-9]+)', PlanController.delete);
+
+routes.get('/registrations', RegistrationController.index);
+routes.post('/registrations', RegistrationController.store);
+routes.put('/registrations/:id', RegistrationController.update);
+routes.delete('/registrations/:id', RegistrationController.delete);
 
 export default routes;
