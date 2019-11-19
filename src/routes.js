f080f6ab8ffdd0f855fe51d5b79ed777562fc410
Antimaterium <vitorafonso33@gmail.com>
2019-11-19 00:27:35 -0200

    Remove '^' char from route

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ routes.put('/students/:id', StudentController.update);
 
 routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
-routes.put('^/plans/:id([0-9]+)', PlanController.update);
+routes.put('/plans/:id([0-9]+)', PlanController.update);
 routes.delete('/plans/:id([0-9]+)', PlanController.delete);
 
 export default routes;
